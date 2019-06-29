d5ec8d9d1e942f8962a172eca69bd04add70d3ec
Hijtec <m.cernil@gmail.com>
2019-06-29 10:48:49 +0530

    Update llc.py
    Hopefully finished

--- src/robot_llc_gpio/src/llc.py
+++ src/robot_llc_gpio/src/llc.py
@@ -7,20 +7,72 @@ import time
 #Messages
 from std_msgs.msg import Float32
 
-#Init
-rospy.init_node('llc_encoder')
-#Publishers
-l_wheel_ticks_pub = rospy.Publisher('l_wheel_ticks',Float32, queue_size = 10)
-r_wheel_ticks_pub = rospy.Publisher('r_wheel_ticks',Float32, queue_size = 10)
-
-#Setup
-GPIO.setmode(GPIO.BCM)
-left = 4
-right = 17
-GPIO.setup(left, GPIO.IN)
-GPIO.setup(right, GPIO.IN)
-
-while 1:
-	l_wheel_ticks_pub.publish(GPIO.input(left))
-	r_wheel_ticks_pub.publish(GPIO.input(right))
+class LLCencoder:
+	def __init__(self):
+		rospy.init_node('llc_encoder')
+		#Publishers
+		self.l_wheel_ticks_pub = rospy.Publisher('l_wheel_ticks',Float32, queue_size = 10)
+		self.r_wheel_ticks_pub = rospy.Publisher('r_wheel_ticks',Float32, queue_size = 10)
+		#Setup
+		self.rate=50
+		#GPIO
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.l_wheel_enc_pin, GPIO.IN)
+		GPIO.setup(self.r_wheel_enc_pin, GPIO.IN)
+		#Initialiazation
+		self.l_wheel_enc_pin = 4
+		self.r_wheel_enc_pin = 17
+		self.l_wheel_ticks_count = 0
+		self.r_wheel_ticks_count = 0
+		self.l_wheel_state_previous = GPIO.input(self.l_wheel_enc_pin)
+		self.l_wheel_state_previous = GPIO.input(self.r_wheel_enc_pin)
+		
+		
+	def change_state(self,state,previous):
+		if state != previous:
+			change = True
+			previous = state
+			return change, previous
+			time.sleep(0.001)
+		else:
+			change = False
+			return change, previous
+	
+	def tick_count(self,state,change,previous,count_ticks):
+		if state == 0 and change == True and previous == 0:
+			count_ticks+=count_ticks
+		return count_ticks
+	
+	def spin(self):
+		rospy.loginfo("Start LLC encoder")
+		rospy.on_shutdown(self.shutdown)
+		while not rospy.is_shutdown():
+			#LEFT ENCODER
+			self.l_wheel_state = GPIO.input(self.l_wheel_enc_pin)
+			self.l_wheel_change, self.l_wheel_state_previous = self.change_state(self.l_wheel_state, self.l_wheel_state_previous)
+			self.l_wheel_ticks_count = self.tick_count(self.l_wheel_state, self.l_wheel_change, self.l_wheel_state_previous, self.l_wheel_ticks_count)
+			#RIGHT ENCODER
+			self.r_wheel_state = GPIO.input(self.r_wheel_enc_pin)
+			self.r_wheel_change, self.r_wheel_state_previous = self.change_state(self.r_wheel_state, self.r_wheel_state_previous)
+			self.r_wheel_ticks_count = self.tick_count(self.r_wheel_state, self.r_wheel_change, self.r_wheel_state_previous, self.r_wheel_ticks_count)
+			#PUBLISHING
+			self.publish()
+		rospy.spin();
+			
+	def shutdown(self):
+		rospy.loginfo("Stop LLC encoder")
+		self.l_wheel_ticks_pub.publish(0)
+		self.r_wheel_ticks_pub.publish(0)
+		rospy.sleep(1)
+		
+	def publish(self):
+		self.l_wheel_ticks_pub.publish(self.l_wheel_ticks_count)
+		self.r_wheel_ticks_pub.publish(self.r_wheel_ticks_count)
+		
+def main():
+	encoder = LLCencoder();
+	encoder.spin()
+	
+if __name__ = '__main__':
+	main();
 
