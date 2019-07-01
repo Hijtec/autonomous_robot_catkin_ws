ba2d4fe2e28c49e2eaad541eeef2ccf3abc4e58b
Hijtec <kulichusik@seznam.cz>
2019-07-01 12:56:37 +0530

    Add files via upload

--- src/robot_llc_gpio/src/llc_gpio.py
+++ src/robot_llc_gpio/src/llc_gpio.py
@@ -44,74 +44,87 @@ class LLCGPIO:
 		self.r_wheel_ang_vel_motor_sub = rospy.Subscriber('r_wheel_ang_vel_motor', Float32, self.r_wheel_ang_vel_motor_callback)
 		self.l_wheel_direction_sub = rospy.Subscriber('l_wheel_direction', Float32, self.l_wheel_direction_callback)
 		self.r_wheel_direction_sub = rospy.Subscriber('r_wheel_direction', Float32, self.r_wheel_direction_callback)
-     
+     		
+		#Initialization
+		self.l_wheel_ang_vel_motor = 0
+		self.r_wheel_ang_vel_motor = 0
+		self.l_wheel_direction = 1
+		self.r_wheel_direction = 1
+
+
   ###DEFINE READING OF DATA###
 	#Read cmd commands
 	def l_wheel_ang_vel_motor_callback(self, msg):
 		self.l_wheel_ang_vel_motor = msg.data
-  def r_wheel_ang_vel_motor_callback(self, msg):
-    self.r_wheel_ang_vel_motor = msg.data
-  #Read direction commands
-  def l_wheel_direction_callback(self, msg):
-    self.l_wheel_direction = msg.data
-  def r_wheel_direction_callback(self, msg):
-    self.r_wheel_direction = msg.data
+	def r_wheel_ang_vel_motor_callback(self, msg):
+    		self.r_wheel_ang_vel_motor = msg.data
+ 	 #Read direction commands
+	def l_wheel_direction_callback(self, msg):
+    		self.l_wheel_direction = msg.data
+  	def r_wheel_direction_callback(self, msg):
+    		self.r_wheel_direction = msg.data
   
-  #Motor command to PWM update
-  def angvelmotor_2_setPWM(self,wheel = 'left'):
-    if wheel == 'left':
-      self.l_pwm.ChangeDutyCycle(self.l_wheel_ang_vel_motor)
-    if wheel == 'right':
-      self.r_pwm.ChangeDutyCycle(self.r_wheel_ang_vel_motor)
+  	#Motor command to PWM update
+  	def angvelmotor_2_setPWM(self,wheel = 'left'):
+    		if wheel == 'left':
+    			self.l_pwm.ChangeDutyCycle(self.l_wheel_ang_vel_motor)
+    		if wheel == 'right':
+      			self.r_pwm.ChangeDutyCycle(self.r_wheel_ang_vel_motor)
   
-  #Motor pins config
-  def setpins(self, wheel = 'left', direction):
-    if wheel == 'left' and direction == 1:
-      GPIO.output(self.l_input1_motor, GPIO.HIGH)
-      GPIO.output(self.l_input2_motor, GPIO.LOW)
-    if wheel == 'left' and direction == 0:
-      GPIO.output(self.l_input1_motor, GPIO.LOW)
-      GPIO.output(self.l_input2_motor, GPIO.HIGH)
-     
-    if wheel == 'right' and direction == 1:
-      GPIO.output(self.r_input1_motor, GPIO.HIGH)
-      GPIO.output(self.r_input2_motor, GPIO.LOW)
-    if wheel == 'right' and direction == 0:
-      GPIO.output(self.r_input1_motor, GPIO.LOW)
-      GPIO.output(self.r_input2_motor, GPIO.HIGH)
+  	#Motor pins config
+  	def setpins(self, direction, wheel = 'left') :
+		direction = int(direction)
+    		if wheel == 'left' and direction == 1:
+      			GPIO.output(self.l_input1_motor, GPIO.HIGH)
+      			GPIO.output(self.l_input2_motor, GPIO.LOW)
+			print('lf')
+    		if wheel == 'left' and direction == 0:
+      			GPIO.output(self.l_input1_motor, GPIO.LOW)
+      			GPIO.output(self.l_input2_motor, GPIO.HIGH)
+			print('lb')
     
-    else:
-      print('Wrong Wheel Direction SET')
+   		if wheel == 'right' and direction == 1:
+      			GPIO.output(self.r_input1_motor, GPIO.HIGH)
+      			GPIO.output(self.r_input2_motor, GPIO.LOW)
+			print('rf')
+    		if wheel == 'right' and direction == 0:
+     			GPIO.output(self.r_input1_motor, GPIO.LOW)
+    		  	GPIO.output(self.r_input2_motor, GPIO.HIGH)
+			print('rb')
     
-  def l_wheel_update(self):
-    self.angvelmotor_2_setPWM('left')
-    self.setpins('left',self.l_wheel_direction)
+    		else:
+			print('Wrong Wheel Direction SET')
+			print(wheel, direction)
+    
+  	def l_wheel_update(self):
+    		self.angvelmotor_2_setPWM('left')
+    		self.setpins(direction = self.l_wheel_direction, wheel = 'left')
       
-  def r_wheel_update(self):
-    self.angvelmotor_2_setPWM('right')
-    self.setpins('right',self.r_wheel_direction)
+  	def r_wheel_update(self):
+    		self.angvelmotor_2_setPWM('right')
+    		self.setpins(direction = self.r_wheel_direction, wheel = 'right')
     
-  def spin(self):
-    rospy.loginfo("Start llc_gpio")
-    rate = rospy.Rate(self.rate)
-    rospy.on_shutdown(self.shutdown)
+  	def spin(self):
+    		rospy.loginfo("Start llc_gpio")
+    		rate = rospy.Rate(self.rate)
+    		rospy.on_shutdown(self.shutdown)
 
-    while not rospy.is_shutdown():
-      self.r_wheel_update()
-      self.l_wheel_update()
-      rate.sleep()
-    rospy.spin();
-      
-  def shutdown(self):
-    rospy.loginfo("Stop llc_gpio")
-    GPIO.cleanup()
-    rospy.loginfo("GPIO cleanup complete")
-    rospy.sleep(1)
+    		while not rospy.is_shutdown():
+      			self.r_wheel_update()
+      			self.l_wheel_update()
+      			rate.sleep()
+  		rospy.spin();
+    
+	def shutdown(self):
+    		rospy.loginfo("Stop llc_gpio")
+    		GPIO.cleanup()
+    		rospy.loginfo("GPIO cleanup complete")
+    		rospy.sleep(1)
       
 def main():
 	llcgpio = LLCGPIO();
 	llcgpio.spin()
 
-if __name__ = '__main__':
-  main();
+if __name__ == '__main__':
+	main();
    
